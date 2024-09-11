48ce9e2d034c454738a2130bbf5416568be70a77
Nyjako <kacper.tucholski.kt@gmail.com>
2024-09-11 18:43:52 +0200

    Added should fail test

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use nyjako_os::println;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -16,8 +17,6 @@ pub extern "C" fn _start() -> ! {
     test_main();
     loop {}
 }
-
-use nyjako_os::println;
 
 #[test_case]
 fn test_println() {
